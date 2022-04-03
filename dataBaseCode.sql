1fb037400384d0c175bf4cd40f02b34fc1789aef
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-04-03 07:57:23 -0300

    Add files via upload

---
+++ dataBaseCode.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clients (
+
+	id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+	name varchar(256) not null, 
+	email varchar(256) not null,
+	
+
+);
+
+INSERT INTO clients (id, name, email) VALUES ('1', 'Melo', 'joaovictordemelo@ufmg.br' );
+
+
+
