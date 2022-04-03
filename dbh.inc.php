fa4a71ccf67263884e6c328a5956af5b393ae04b
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-04-03 07:55:56 -0300

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,13 @@
+<?php 
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = '';
+$dbName = "clientsAgenda";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+
+?>
