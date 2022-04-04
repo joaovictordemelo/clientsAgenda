d43aaf178fafb928db351141048fb4064274c059
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-04-04 05:24:46 -0300

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -6,9 +6,13 @@
 	$id = $_POST['id'];
 	$name = $_POST['name'];
 	$email = $_POST['email'];
+	$job = $_POST['job'];
+	$market = $_POST['market'];
+	$phone = $_POST['phone'];
+	$address = $_POST['address'];
 
 
-	$sql = "INSERT INTO clients (id, name, email) VALUES ('$id', '$name', '$email');";
+	$sql = "INSERT INTO clients (id, name, email,job, market, phone, address) VALUES ('$id', '$name', '$email', '$job', '$market', '$phone', '$address');";
 
 	mysqli_query($conn, $sql);
 
