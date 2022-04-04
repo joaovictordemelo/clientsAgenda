bcff404dea4f3d04ce2f4ef98718c74ba7824c2e
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-04-04 05:25:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,24 +8,60 @@
 
 <html>
 <head>
+	<style>
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  color: white;
+  text-align: center;
+}
+
+
+
+
+</style>
+
+	<link rel= "stylesheet" type = "tex/css" href="formclient.css">
 	<title>Welcome to Adamastor</title>
 </head>
 <body>
 
+	<div>
+		<h1 id = "title"> Agenda Adamastor, Hi friend </h1>
+		<p > Diga mais sobre você! </p>
 
-<form action = "signup.inc.php" method = "POST">
-	<input type = "text" name = "id" placeholder="id">
+	</div>
+
+
+<form   style="text-align: center; " action = "signup.inc.php" method = "POST">
+	<input  type = "text" name = "id" placeholder="id" required>
 	<br>
-	<input type = "text" name = "name" placeholder="name">
+	<input type = "text" name = "name" placeholder="name" required>
 	<br>
-	<input type = "text" name = "email" placeholder="email">
+	<input type = "text" name = "email" placeholder="email" required>
 	<br>
+	<input type = "text" name = "job" placeholder="job">
+	<br>
+	<input type = "text" name = "market" placeholder="market">
+	<br>
+	<input type = "tel" name = "phone" placeholder="phone" required>
+	<br>
+	<input type = "text" name = "address" placeholder="address">
+	<br>
+
 	<button type="submit" name = "submit">Enter </button>
 
 </form>
 
 <?php
-	$sql = "INSERT INTO clients (id, name, email) VALUES ('1', 'Melo', 'joaovictordemelo@ufmg.br' );";
+	$sql = "INSERT INTO clients (id, name, email,job, market, phone, address) VALUES ('1', 'joao', 'joaovictordemelo@ufmg.br', 'student', 'universidade', '96861197', 'avenida das palmeiras');";
 
 	mysqli_query($conn, $sql);
 	
